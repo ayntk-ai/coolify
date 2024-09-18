1d680369e14aea457b793ac59852b9f0e5c747cb
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2024-09-18 22:01:53 +0200

    Feat: Start proxy when seeding the database

--- database/seeders/ServerSeeder.php
+++ database/seeders/ServerSeeder.php
@@ -3,13 +3,14 @@
 namespace Database\Seeders;
 
 use App\Models\Server;
+use App\Enums\ProxyTypes;
 use Illuminate\Database\Seeder;
 
 class ServerSeeder extends Seeder
 {
     public function run(): void
     {
-        Server::create([
+        $server = Server::create([
             'id' => 0,
             'name' => 'localhost',
             'description' => 'This is a test docker container in development mode',
@@ -17,5 +18,11 @@ class ServerSeeder extends Seeder
             'team_id' => 0,
             'private_key_id' => 0,
         ]);
+
+        // Set up proxy status without actually starting it
+        // if you just run spin up the proxy wil lstart autmitcally becuase of the server check job (if we seeded the DB once with the new seeder)
+        $server->proxy->set('status', 'exited');
+        $server->proxy->set('type', ProxyTypes::TRAEFIK->value);
+        $server->save();
     }
 }
---
+++ database/seeders/StartProxySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Server;
+use App\Actions\Proxy\StartProxy;
+use Illuminate\Database\Seeder;
+use App\Events\ProxyStatusChanged;
+
+class StartProxySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $server = Server::find(0); // Assuming the server ID is 0 as in ServerSeeder
+
+        if ($server) {
+            // This starts the proxy but it does not wait for it to finish starting (it is instat though) 
+            // If we want to wait for the proxy we would have to add something like this: StartProxy::run($server, async: false, force: true);
+            // Also if the proxy is running it will remove it and start a new one.
+            $server->proxy->force_stop = false;
+            $server->save();
+            $activity = StartProxy::run($server, force: true);
+            dispatch('activityMonitor', $activity->id, ProxyStatusChanged::class);
+        }
+    }
+}
